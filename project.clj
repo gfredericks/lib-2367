3e9782424fe01a1cee4cb8363a228b49f7a5159a
Gary Fredericks <fredericksgary@gmail.com>
2011-11-10 16:49:55 -0500

    Clojure 1.3 I guess? I don't really know what I'm doing...

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject lib-2367 "0.0.1-SNAPSHOT"
   :description "Class generation utilities for Clojure."
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [inflections "0.6.3"]])
